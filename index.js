9d2cee68ff3bf38e833f1cd7dda0e8a6ad8884e4
marcelo lima do nascimento <30661697+marcklima@users.noreply.github.com>
2022-04-08 23:55:12 -0300

    v4
    v4

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ appServer.get("/", function (req,res){
   res.send("OI, Seja bem vindo!");
 });
 
+appServer.get("/canal/youtube", function(req, res){
+  const canal = req.query["canal"];
+  if (canal){
+    res.send("<h1> Bem Vindo ao site !! ! </h1>By <h2>")
+}else {
+  res.send("<h3>Site não recebeu parametros<p> tente novamente</h3>")
+}
+});
+
 appServer.get("/home/:nome?", function(req,res){
 var nome = req.params.nome;
 
---
+++ teste.sql
@@ -0,0 +1,5 @@
+INSERT INTO  usuarios (nome, email, idade) VALUES(
+  "MARCELO LIMA",
+  "marcklima@gmail.com",
+  45
+);
