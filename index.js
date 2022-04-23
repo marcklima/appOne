3832afc87e6ec020da53f4203f075c2a20c1418b
marcelo lima do nascimento <30661697+marcklima@users.noreply.github.com>
2022-04-23 20:36:46 -0300

    fgdfg
    dfgdfg

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
+
 const express = require('express');
 const appServer = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
-const Quest = require("./database/Quest")
-
+const Quest = require("./database/Quest");
+const Resposta = require("./database/Resposta");
 
 connection
   .authenticate()
-  .then(() =>{
+  .then(() => {
     console.log("bd conectado")
   })
-  .catch((msgErro) =>{
+  .catch((msgErro) => {
     console.log(msgErro);
-  } )
-
+  })
+//chamada para o express usar o EJS com view egine
 appServer.set('view engine', 'ejs'); // renderizador de html será o ejs
 appServer.use(express.static('public'));
-
-appServer.use(bodyParser.urlencoded({extended:false})); //responsavel para receber e traduzir o envior do metodo post
+//body parser
+appServer.use(bodyParser.urlencoded({ extended: false })); //responsavel para receber e traduzir o envior do metodo post
 appServer.use(bodyParser.json());
+//rotas
 
-
-
-
-//start
-appServer.get('/quest', (req, res) => {
-  res.render('quest');
-});//fim 
-
-
-//start
 appServer.get("/", (req, res) => {
   var nome = "Marcelo Lima";
   var lang = "Java Script";
@@ -38,39 +30,17 @@ appServer.get("/", (req, res) => {
     nome: nome,
     empresa: "Baináu",
     lang: lang,
-    insvritos: 8040
+    inscritos: 8040
   })
 
 });//fim
 
-/*
-appServer.get("/", function (req,res){
-  res.send("OI, Seja bem vindo!");
-});
+//start
+appServer.get('/quest', (req, res) => {
+  res.render('quest');
+});//fim 
 
 
-appServer.get("/canal/youtube", function(req, res){
-  const canal = req.query["canal"];
-  if (canal){
-    res.send("<h1> Bem Vindo ao site !! ! </h1>By <h2>")
-}else {
-  res.send("<h3>Site não recebeu parametros<p> tente novamente</h3>")
-}
-});
-
-
-appServer.get("/home/:nome?", function(req,res){
-var nome = req.params.nome;
-
-  if (nome){
-    res.send("<h1> Hello "+nome+" Seja Bem vindo!!!</h1>")
-
-  }else {
-    res.send("<h1>Bem vindo ao Blog Baináu")
-  }
-});
-
-*/
 //start
 appServer.post('/savequest', (req, res) => {
   var titulo = req.body.titulo;
@@ -81,16 +51,16 @@ appServer.post('/savequest', (req, res) => {
     descricao: descricao
   }).then(() => {
     res.redirect("/");
-  
-});//fim
+  })
+  });//fim
 
 
 
-appServer.listen(8181, function (erro) {
-  if (erro) {
-    console.log("CRASH!!!, erro em subir o servidor !!!")
-  } else {
-    console.log("UHUUU, Servidor on line !!!")
-  }//fim do else
+  appServer.listen(8181, function (erro) {
+    if (erro) {
+      console.log("CRASH!!!, erro em subir o servidor !!!")
+    } else {
+      console.log("UHUUU, Servidor on line !!!")
+    }//fim do else
 
-});
+  });
